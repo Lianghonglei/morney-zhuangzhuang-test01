07124dd3ea2d4be36d4bc90fc9b81a42821837cd
lei <895641390@qq.com>
2020-12-19 15:57:30 +0800

    创建三个路页面

---
+++ src/views/Statistics.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>statistics.vue</div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'Statistics'
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
