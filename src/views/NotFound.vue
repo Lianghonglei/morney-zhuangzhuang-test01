eff5c66244fc561132e5daa6a2b12d7dd60b3301
lei <895641390@qq.com>
2020-12-19 16:33:00 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>Page is not found!</div>
+        <router-link to="/">Return to the home page!</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NotFount'
+    };
+</script>
+
+<style scoped lang="scss">
+
+</style>
