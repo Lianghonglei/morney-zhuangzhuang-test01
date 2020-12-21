21601d091b2206f097d4f943aed7f92c82ad6071
lei <895641390@qq.com>
2020-12-21 21:03:04 +0800

    创建<Icon/>组件ï¼++样式+切换路由样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="nav">
-        <router-link to="/labels">
+    <nav>
+        <router-link to="/labels" class="item" active-class="selected">
             <Icon name="label"/>
             标签
         </router-link>
-        <router-link to="/money">
+        <router-link to="/money" class="item" active-class="selected">
             <Icon name="money"/>
             记账
         </router-link>
-        <router-link to="/statistics">
+        <router-link to="/statistics" class="item" active-class="selected">
             <Icon name="statistics"/>
             统计
         </router-link>
-    </div>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -22,7 +22,25 @@
 </script>
 
 <style scoped lang="scss">
-    .nav{
-        border: 1px solid red;
+    nav{
+        display: flex;
+        box-shadow: 0 0 3px rgba(0,0,0,0.25);
+        flex-direction: row;
+        font-size: 12px;
+        > .item{
+            padding: 2px 0;
+            width: 33.333333%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
+        .selected{
+            color: chocolate;
+        }
     }
 </style>
