13649757553f7ba694d03899da6501bffd9955a8
lei <895641390@qq.com>
2021-01-31 17:37:35 +0800

    删除Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,7 +19,6 @@
     import Vue from 'vue';
     import Tags from '@/components/Money/Tags.vue';
     import FormItem from '@/components/Money/FormItem.vue';
-    import Types from '@/components/Money/Types.vue';
     import NumberPad from '@/components/Money/NumberPad.vue';
     import {Component} from 'vue-property-decorator';
     import Tabs from '@/components/Tabs.vue';
@@ -29,7 +28,6 @@
         components: {
             Tabs,
             NumberPad,
-            Types,
             FormItem,
             Tags,
         }
